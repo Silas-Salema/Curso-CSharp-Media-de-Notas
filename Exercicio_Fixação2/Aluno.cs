9a0ff6357a02c51066c1ed71a0f3482ca19f6540
Silas Salema <silazdasilva20@gmail.com>
2019-12-20 10:40:06 -0300

    Media de Notas de Alunos

---
+++ Exercicio_Fixação2/Aluno.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Exercicio_Fixação2
+{
+    class Aluno
+    {
+        public string Nome;
+        public double N1;
+        public double N2;
+        public double N3;
+        
+        public double CalculaMedia(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+
+        public void Aprovacao(double d)
+        {
+            if (d >= 60)
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                double f = Faltou(d);
+                Console.WriteLine("FALTARAM: " + f.ToString("F2", CultureInfo.InvariantCulture) + " PONTOS");
+            }
+        }
+
+        public double Faltou(double e)
+        {
+            return 60 - e;
+        }
+        
+    }
+}
---
+++ Exercicio_Fixação2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_Fixação2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno a1 = new Aluno();
+
+            Console.Write("Nome do aluno: ");
+            a1.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as 3 notas do aluno:");
+            a1.N1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a1.N2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            a1.N3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double media = a1.CalculaMedia(a1.N1, a1.N2, a1.N3);
+            Console.WriteLine("NOTA FINAL: " + media.ToString("F2", CultureInfo.InvariantCulture));
+            a1.Aprovacao(media);
+        }
+    }
+}
